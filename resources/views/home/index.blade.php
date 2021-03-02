75b6e8182c6a3da3b996dd5fa632f3b346fc6906
theoboldalex <theoboldalex@gmail.com>
2021-03-02 10:20:54 +0000

    latest posts grid

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,10 +5,12 @@
         <h1 class="font-semibold text-3xl my-6">hello {{ auth()->user()->username }}</h1>
     @endauth
     <h2 class="font-semibold text-2xl my-6">Latest uploads</h2>
-    @foreach ($records as $record)
-        <div class="border rounded-lg p-4">
-            <h4 class="font-semibold text-xl">{{ $record->title }}</h4>
-            <p>{{ $record->artist }}</p>
-        </div>
-    @endforeach
+    <div class="card-grid">
+        @foreach ($records as $record)
+            <div class="border rounded-lg p-4 w-full">
+                <h4 class="font-semibold text-xl">{{ $record->title }}</h4>
+                <p>{{ $record->artist }}</p>
+            </div>
+        @endforeach
+    </div>
 @endsection
