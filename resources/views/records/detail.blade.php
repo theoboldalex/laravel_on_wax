c58cd66ccb6ec9dfec05fcf34cc64cbd3f0c2623
theoboldalex <theoboldalex@gmail.com>
2021-03-19 23:02:30 +0000

    back button

--- resources/views/records/detail.blade.php
+++ resources/views/records/detail.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-  <section class="my-8">
-    <h1 class="font-semibold text-4xl">
-      {{ $record->title }}
-    </h1>
-    <h1 class="font-semibold text-2xl opacity-70">{{ $record->artist }}</h1>
-      <img src="{{ asset('storage/records/' . $record->image) }}" alt="album art for {{ $record->title }} by {{ $record->artsist }}" class="my-4">
-  </section>
+    <section class="my-6">
+        <div class="my-4">
+            <a href="{{ url()->previous(route('home')) }}" class="text-primary hover:opacity-70 transition duration-300 ease-in-out">Back</a>
+        </div>
+        <h1 class="font-semibold text-4xl">
+            {{ $record->title }}
+        </h1>
+        <h1 class="font-semibold text-2xl opacity-70">{{ $record->artist }}</h1>
+        <img src="{{ asset('storage/records/' . $record->image) }}"
+             alt="album art for {{ $record->title }} by {{ $record->artsist }}" class="my-4">
+    </section>
 @endsection
