d439ef38f3252d843419dda8f447aeaca8a42242
theoboldalex <theoboldalex@gmail.com>
2021-03-24 19:56:56 +0000

    show avatar

--- resources/views/users/edit_profile.blade.php
+++ resources/views/users/edit_profile.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>{{ auth()->user() }}</h1>
-        <img src="{{ asset('storage/avatars/' . auth()->user()->avatar) }}" alt="">
+        <img src="{{ asset('storage/avatar/' . auth()->user()->avatar) }}" alt="{{ auth()->user()->username }}'s avatar" width="200" class="rounded-full">
 {{--    <form action="{{ route('edit_profile', auth()->user()->username) }}" method="POST" enctype="multipart/form-data">--}}
 {{--    </form>--}}
 @endsection
