9ad934c6981c0b19712fc2b9df3eac3ba8b98e26
theoboldalex <theoboldalex@gmail.com>
2021-03-24 20:26:05 +0000

    remove user dump and add page heading

--- resources/views/users/edit_profile.blade.php
+++ resources/views/users/edit_profile.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ auth()->user() }}</h1>
+    <h1 class="font-semibold text-4xl my-8">Edit Profile</h1>
         <img src="{{ asset('storage/avatar/' . auth()->user()->avatar) }}" alt="{{ auth()->user()->username }}'s avatar" width="200" class="rounded-full">
     <form action="{{ route('edit_profile', auth()->user()->username) }}" method="POST" enctype="multipart/form-data">
         @csrf
