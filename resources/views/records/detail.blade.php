1c5b0df59ee5f016b6be84bb8969879bbf633ef5
theoboldalex <theoboldalex@gmail.com>
2021-04-01 19:47:16 +0100

    basic comment card

--- resources/views/records/detail.blade.php
+++ resources/views/records/detail.blade.php
@@ -56,6 +56,20 @@
         <hr>
         <div class="flex flex-col my-4">
             <h2 class="font-semibold text-3xl">Comments</h2>
+
+            <div class="flex flex-col my-4">
+                <div class="border rounded-xl p-8">
+                    <div class="flex items-center mb-4">
+                        <img src="{{ asset('/storage/avatar/0kuetINANFXPLMRLochp3ApksEr9A7WbA6i45Kg7.jpg') }}" alt="" width="50" class="rounded-full mr-4">
+                        <h4 class="font-semibold mr-4">folky_dokey</h4>
+                        <h4 class="text-gray-400">1 day ago</h4>
+                    </div>
+                    <hr>
+                    <div class="font-light mt-4">
+                        Awesome dude!
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
