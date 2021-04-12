9ff04689d919c5ddb3c4453464791d70295195e3
theoboldalex <theoboldalex@gmail.com>
2021-04-12 21:05:44 +0100

    another tweak

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <section>
-        <div class="flex flex-col my-8">
+        <div class="flex my-8">
             <div class="md:w-4/12 flex justify-center items-center">
                 <img src="{{ Storage::disk('s3')->url('public/avatar/' . $user->avatar) }}" alt="profile image" width="200" class="rounded-full">
             </div>
-            <div class="md:w-8/12 flex items-center justify-center">
+            <div class="md:w-8/12 flex flex-col items-center justify-center">
                 <div class="flex justify-center items-center">
                     <h1 class="font-semibold text-3xl mr-8">{{ $user->username }}</h1>
                     @auth
