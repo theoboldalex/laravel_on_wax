47cfe00894225cfffcc4cbec5e78dc571bb29065
theoboldalex <theoboldalex@gmail.com>
2021-08-07 13:47:46 +0100

    dd isfollowing

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -26,7 +26,7 @@ class ProfileController extends Controller
             $isFollowing = false;
         }
 
-        dd($user->followers);
+        dd($isFollowing);
         return view('users.profile', [
             'user' => $user,
             'isFollowing' => $isFollowing
