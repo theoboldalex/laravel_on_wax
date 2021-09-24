3921d5e44e2033d2c01ea1dc0dcc822f528f800d
theoboldalex <theoboldalex@gmail.com>
2021-09-24 19:51:14 +0100

    test get record by id

--- tests/Unit/RoutesTest.php
+++ tests/Unit/RoutesTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\Record;
 use Tests\TestCase;
 
 class RoutesTest extends TestCase
@@ -10,5 +11,20 @@ class RoutesTest extends TestCase
     {
         $response = $this->get('/');
         $response->assertOk();
+    }
+
+    public function testGetRecordById()
+    {
+        $record = Record::create([
+            'title' => 'TEST_TITLE',
+            'artist' => 'TEST_ARTIST',
+            'user_id' => 5
+        ]);
+
+        $response = $this->get('/records/'.$record->id);
+
+        $response->assertOk();
+        $this->assertEquals('TEST_TITLE', $record->title);
+        $this->assertEquals('TEST_ARTIST', $record->artist);
     }
 }
