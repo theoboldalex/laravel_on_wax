ac3b99536c70900b421ab796d3e9b95c9caf0754
theoboldalex <theoboldalex@gmail.com>
2021-09-25 19:14:26 +0100

    tested a user can log in and be authenticated

--- tests/Feature/RoutesTest.php
+++ tests/Feature/RoutesTest.php
@@ -4,12 +4,30 @@ namespace Tests\Feature;
 
 use App\Models\Record;
 use App\Models\User;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 use Tests\TestCase;
 
 class RoutesTest extends TestCase
 {
     use DatabaseTransactions;
+
+    protected Collection|Model|Authenticatable $user;
+    protected string $password;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Session::start();
+        $this->user = User::factory()->create([
+            'password' => Hash::make($this->password = 'test1234')
+        ]);
+    }
 
     public function testGetIndexPageIsOk()
     {
@@ -19,8 +37,7 @@ class RoutesTest extends TestCase
 
     public function testGetRecordById()
     {
-        $user = User::factory()->create();
-        $record = Record::factory()->for($user)->create();
+        $record = Record::factory()->for($this->user)->create();
 
         $response = $this->get("/records/{$record->id}");
 
@@ -31,20 +48,39 @@ class RoutesTest extends TestCase
 
     public function testGetUsersRecords()
     {
-        $user = User::factory()->create();
-        $userRecords = Record::factory()->count(5)->for($user)->create();
+        $userRecords = Record::factory()->count(5)->for($this->user)->create();
 
-        $response = $this->get("/users/{$user->username}");
+        $response = $this->get("/users/{$this->user->username}");
         $response->assertOk();
 
         $this->assertEquals(5, $userRecords->count());
 
-        $view = $this->view('users.profile', ['user' => $user]);
-        $view->assertSee($user->username);
+        $view = $this->view('users.profile', ['user' => $this->user]);
+        $view->assertSee($this->user->username);
 
         foreach ($userRecords as $record) {
-            $this->assertEquals($record->user_id, $user->id);
+            $this->assertEquals($record->user_id, $this->user->id);
             $view->assertSee($record->id);
         }
+    }
+
+    public function testUserCanLogin()
+    {
+        $credentials = [
+            'email' => $this->user->email,
+            'password' => $this->password,
+            '_token' => csrf_token()
+        ];
+
+        $response = $this->call(
+            'POST',
+            '/auth/login',
+            $credentials
+        );
+
+        $response->assertRedirect('/');
+        $response->assertSessionHasNoErrors();
+        $response->assertSessionMissing(['status' => 'Invalid credentials']);
+        $this->assertAuthenticatedAs($this->user);
     }
 }
