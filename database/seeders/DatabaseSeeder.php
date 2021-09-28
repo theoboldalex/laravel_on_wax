8b477e4f91b9873108047f115e4b88e7637b72b6
theoboldalex <theoboldalex@gmail.com>
2021-09-28 20:59:35 +0100

    basic db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Record;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->count(10)
+            ->has(
+                Record::factory()->count(5)
+            )->create();
     }
 }
