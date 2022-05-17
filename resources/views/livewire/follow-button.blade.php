a8bf2bd325a649098ec4b906496a1ee7b762cc7e
theoboldalex <theoboldalex@gmail.com>
2022-05-17 20:08:18 +0100

    Remove redundant code

--- resources/views/livewire/follow-button.blade.php
+++ resources/views/livewire/follow-button.blade.php
@@ -1,14 +1,5 @@
 @auth
     @if(auth()->user()->username != request()->route('username'))
-        {{--        <form--}}
-        {{--            action="{{ $isFollowing ? route('unfollow', $user->username) : route('follow', $user->username) }}"--}}
-        {{--            method="post">--}}
-        {{--            @csrf--}}
-        {{--            <button type="submit"--}}
-        {{--                    class="px-4 py-2 bg-primary text-white rounded-lg hover:opacity-70 transition duration-300 ease">{{ $buttonText }}--}}
-        {{--            </button>--}}
-        {{--        </form>--}}
-
         <button wire:click="toggleFollowing"
                 class="px-4 py-2 bg-primary text-white rounded-lg hover:opacity-70 transition duration-300 ease">{{ $buttonText }}
         </button>
